1612e48ab26bb6357fbb82e45c5bc602b644f48d
Edward Hesketh <headblockhead@gmail.com>
2024-04-22 17:26:47 +0100

    feat: remove led blink task, add submodule

--- src/main.c
+++ src/main.c
@@ -1,28 +1,3 @@
-/*
- * The MIT License (MIT)
- *
- * Copyright (c) 2019 Ha Thach (tinyusb.org)
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- *
- */
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -32,27 +7,8 @@
 
 #include "usb_descriptors.h"
 
-//--------------------------------------------------------------------+
-// MACRO CONSTANT TYPEDEF PROTYPES
-//--------------------------------------------------------------------+
-
-/* Blink pattern
- * - 250 ms  : device not mounted
- * - 1000 ms : device mounted
- * - 2500 ms : device is suspended
- */
-enum {
-  BLINK_NOT_MOUNTED = 250,
-  BLINK_MOUNTED = 1000,
-  BLINK_SUSPENDED = 2500,
-};
-
-static uint32_t blink_interval_ms = BLINK_NOT_MOUNTED;
-
-void led_blinking_task(void);
 void hid_task(void);
 
-/*------------- MAIN -------------*/
 int main(void) {
   board_init();
 
@@ -61,36 +17,26 @@ int main(void) {
 
   while (1) {
     tud_task(); // tinyusb device task
-    led_blinking_task();
 
     hid_task();
   }
 }
 
-//--------------------------------------------------------------------+
-// Device callbacks
-//--------------------------------------------------------------------+
-
 // Invoked when device is mounted
-void tud_mount_cb(void) { blink_interval_ms = BLINK_MOUNTED; }
+void tud_mount_cb(void) {}
 
 // Invoked when device is unmounted
-void tud_umount_cb(void) { blink_interval_ms = BLINK_NOT_MOUNTED; }
+void tud_umount_cb(void) {}
 
 // Invoked when usb bus is suspended
 // remote_wakeup_en : if host allow us  to perform remote wakeup
 // Within 7ms, device must draw an average of current less than 2.5 mA from bus
-void tud_suspend_cb(bool remote_wakeup_en) {
-  (void)remote_wakeup_en;
-  blink_interval_ms = BLINK_SUSPENDED;
-}
+void tud_suspend_cb(bool remote_wakeup_en) { (void)remote_wakeup_en; }
 
 // Invoked when usb bus is resumed
-void tud_resume_cb(void) { blink_interval_ms = BLINK_MOUNTED; }
-
-//--------------------------------------------------------------------+
-// USB HID
-//--------------------------------------------------------------------+
+void tud_resume_cb(void) {
+  // (mounted)
+}
 
 static void send_hid_report(uint8_t report_id, uint32_t btn) {
   // skip if hid is not ready yet
@@ -138,34 +84,6 @@ static void send_hid_report(uint8_t report_id, uint32_t btn) {
       if (has_consumer_key)
         tud_hid_report(REPORT_ID_CONSUMER_CONTROL, &empty_key, 2);
       has_consumer_key = false;
-    }
-  } break;
-
-  case REPORT_ID_GAMEPAD: {
-    // use to avoid send multiple consecutive zero report for keyboard
-    static bool has_gamepad_key = false;
-
-    hid_gamepad_report_t report = {.x = 0,
-                                   .y = 0,
-                                   .z = 0,
-                                   .rz = 0,
-                                   .rx = 0,
-                                   .ry = 0,
-                                   .hat = 0,
-                                   .buttons = 0};
-
-    if (btn) {
-      report.hat = GAMEPAD_HAT_UP;
-      report.buttons = GAMEPAD_BUTTON_A;
-      tud_hid_report(REPORT_ID_GAMEPAD, &report, sizeof(report));
-
-      has_gamepad_key = true;
-    } else {
-      report.hat = GAMEPAD_HAT_CENTERED;
-      report.buttons = 0;
-      if (has_gamepad_key)
-        tud_hid_report(REPORT_ID_GAMEPAD, &report, sizeof(report));
-      has_gamepad_key = false;
     }
   } break;
 
@@ -249,33 +167,9 @@ void tud_hid_set_report_cb(uint8_t instance, uint8_t report_id,
 
       if (kbd_leds & KEYBOARD_LED_CAPSLOCK) {
         // Capslock On: disable blink, turn led on
-        blink_interval_ms = 0;
-        board_led_write(true);
       } else {
         // Caplocks Off: back to normal blink
-        board_led_write(false);
-        blink_interval_ms = BLINK_MOUNTED;
       }
     }
   }
-}
-
-//--------------------------------------------------------------------+
-// BLINKING TASK
-//--------------------------------------------------------------------+
-void led_blinking_task(void) {
-  static uint32_t start_ms = 0;
-  static bool led_state = false;
-
-  // blink is disabled
-  if (!blink_interval_ms)
-    return;
-
-  // Blink every interval ms
-  if (board_millis() - start_ms < blink_interval_ms)
-    return; // not enough time
-  start_ms += blink_interval_ms;
-
-  board_led_write(led_state);
-  led_state = 1 - led_state; // toggle
 }
