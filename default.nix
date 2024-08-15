b9334c65192a429c707232ab27a862fde68ef982
Edward Hesketh <headblockhead@gmail.com>
2024-08-15 18:05:55 +0100

    feat: add nix building

---
+++ default.nix
@@ -0,0 +1,34 @@
+{ lib
+, gcc-arm-embedded
+, gnumake
+, cmake
+, stdenv
+, picotool
+, python39
+}:
+
+stdenv.mkDerivation {
+  pname = "dc32-badge";
+  version = "0.0.0";
+
+  src = ./.;
+
+  nativeBuildInputs = [ cmake gnumake gcc-arm-embedded picotool ];
+  buildInputs = [ python39 ];
+
+  cmakeFlags = [
+    "-DPICO_PLATFORM=rp2350"
+    "-DCMAKE_C_COMPILER=${gcc-arm-embedded}/bin/arm-none-eabi-gcc"
+    "-DCMAKE_CXX_COMPILER=${gcc-arm-embedded}/bin/arm-none-eabi-g++"
+  ];
+
+  installPhase = ''cp -r . $out'';
+
+  meta = with lib; {
+    homepage = "https://github.com/headblockhead/dc32-badge";
+    description = "Custom firmware built from scratch for the Defcon 32 badge.";
+    licencse = licenses.mit;
+    platforms = with platforms; linux;
+  };
+}
+
--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Tools for developing, building, flashing, and debugging the slab keyboard";
+  description = "Tools for developing and building the dc32-badge";
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
     flake-utils.url = "github:numtide/flake-utils";
@@ -14,15 +14,19 @@
             overlays = [
               (final: prev: {
                 xc = inputs.xc.packages.x86_64-linux.xc;
+                picotool = pkgs.callPackage ./picotool.nix {
+                  pico-sdk = pkgs.callPackage ./pico-sdk.nix { };
+                };
+                pico-sdk = pkgs.callPackage ./pico-sdk.nix { };
               })
             ];
           };
         in
         {
+          # Developmnt shell (nix develop)
           devShells.default = pkgs.mkShell {
             buildInputs = with pkgs; [
               xc
-
               cmake
               gcc
               ccls
@@ -37,6 +41,8 @@
               cacert
             ];
           };
+          # Build
+          packages.dc32-badge = pkgs.callPackage ./default.nix { };
         }
       );
 }
